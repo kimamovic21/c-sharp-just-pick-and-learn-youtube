053b4e66884d6600ab13806273ca148809637b11
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-21 13:55:01 +0100

    Boolean and character data types

--- C# Basics/Program.cs
+++ C# Basics/Program.cs
@@ -1,25 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-class Floating_Point_Demo
+class Boolean_and_character_demo
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("C# floating point data type");
+        Console.WriteLine("Boolean and character data types");
 
-        float f1 = 1.2f;
-        float f2 = 1.3f;
+        // Syntax
+        bool boolVar;
 
-        // Range of float data type
-        Console.WriteLine("Range of float data type: {0} to {1}", float.MinValue, float.MaxValue);
+        // Example 1: Verify TrueString and FalseString
+        Console.WriteLine("Value of TrueString: {0}", bool.TrueString);
+        Console.WriteLine("Value of FalseString: {0}", bool.FalseString);
 
-        // Verify the equality using equals method
-        if (f1.Equals(f2))
-        {
-            Console.WriteLine("Variables f1 and f2 are equals");
-        } else
-        {
-            Console.WriteLine("Variables f1 and f2 are not equals");
-        }
+        // Example 2: isNumber method
+        char charVar = '3';
+        bool result = char.IsNumber(charVar);
+        Console.WriteLine("Is it a number? : {0}", result);
     }
 }
