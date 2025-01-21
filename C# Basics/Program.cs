0fd43be760a84cb7df88651f5747742d3b8768ef
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-21 15:39:42 +0100

    C# String data type

--- C# Basics/Program.cs
+++ C# Basics/Program.cs
@@ -1,26 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-class Object_data_type_demo
+class String_data_type_demo
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Object data type");
+        Console.WriteLine("String data type \n");
 
-        // Declare
-        object obj;
+        // Instantiate a string
+        
+        string name = "C# tutorial";
+        Console.WriteLine("String created by assigning a string literal: {0} \n", name);
 
-        int a = 20;
-        Console.WriteLine(a.GetType());
 
-        string str = "Hello...";
-        obj = str;
-        Console.WriteLine(obj);
+        char[] charArray = { 'C', '#', ' ', 't', 'u', 't', 'o', 'r', 'i', 'a', 'l'};
+        string stringFromChar = new string(charArray);
+        Console.WriteLine("String created by calling a String class constructor: {0} \n", stringFromChar);
 
-        // Boxing - Converting value type to object type
-        obj = a;
+        string string1 = "C#";
+        string string2 = " tutorial";
+        string string3 = string1 + string2;
+        Console.WriteLine("String created by using the string concatenation operator: {0} \n", string3);
 
-        // Unboxing - Converting object type to value type 
-        a = (int)obj;
+        string newName = name.Replace("C#", ".NET");
+        Console.WriteLine("String created by calling a method that returns a string");
+        Console.WriteLine("old value: {0}", name);
+        Console.WriteLine("new value: {0}", newName);
+
+        Console.WriteLine("\nString created by calling a formatting that returns a string");
+        string formattedString = string.Format("old value: {0}, new value {1}", name, newName);
+        Console.WriteLine(formattedString);
+
+        // Operations
+        string originalString = "I am original string...";
+        string modifiedString = originalString.Insert(originalString.Length, " and I am inserted string");
+        Console.WriteLine(originalString);
+        Console.WriteLine(modifiedString);
+
+        string removedString = modifiedString.Remove(originalString.Length);
+        Console.WriteLine(removedString);
+
+        Console.ReadLine();
     }
 }
