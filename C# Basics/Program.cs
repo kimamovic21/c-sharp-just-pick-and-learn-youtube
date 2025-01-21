c84ba890b1302b263d0e50d22de1824716b9c677
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-21 11:04:35 +0100

    Data types in C#

--- C# Basics/Program.cs
+++ C# Basics/Program.cs
@@ -1,11 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-class Compilation_Demo
+class Data_Types
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Compilation in C#");
-        //Console.WriteLine("This is error message in C#")
+        Console.WriteLine("Data types in C#");
     }
 }
