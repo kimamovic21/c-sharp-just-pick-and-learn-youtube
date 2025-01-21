c02cf08d5901c2ec1d14f9ae85432cca01ffa860
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-21 13:15:05 +0100

    C# integer type

--- C# Basics/Program.cs
+++ C# Basics/Program.cs
@@ -1,10 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-class Data_Types
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Data types in C#");
+        Console.WriteLine("C# Integer Type");
+
+        int a;
+
+        // Size of the int data type
+        Console.WriteLine("Size of the int data type is: {0}", sizeof(int));
+
+        // Range of the int data type
+        Console.WriteLine("Range of the int data type is ${0} to {1}", int.MinValue, int.MaxValue);
     }
 }
