a06763e7ce7c31e218b413cd663b41f656dc909a
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-23 13:25:29 +0100

    Logical OR operators in C#

--- C# Basics/Program.cs
+++ C# Basics/Program.cs
@@ -1,18 +1,18 @@
-﻿class Logical_And_Operator
+﻿class Logical_Or_Operator
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Conditional Logical AND operator - &&");
-        Console.WriteLine("Operand1 = true, Operand2 = true, Result={0}", true && true);
-        Console.WriteLine("Operand1 = true, Operand2 = false, Result={0}", true && false);
-        Console.WriteLine("Operand1 = false, Operand2 = true, Result={0}", false && true);
-        Console.WriteLine("Operand1 = false, Operand2 = false, Result={0}", false && false);
+        Console.WriteLine("Conditional Logical OR operator - ||");
+        Console.WriteLine("Operand1 = true, Operand2 = true, Result={0}", true || true);
+        Console.WriteLine("Operand1 = true, Operand2 = false, Result={0}", true || false);
+        Console.WriteLine("Operand1 = false, Operand2 = true, Result={0}", false || true);
+        Console.WriteLine("Operand1 = false, Operand2 = false, Result={0}", false || false);
 
-        Console.WriteLine("\nLogical AND operator - &");
-        Console.WriteLine("Operand1 = true, Operand2 = true, Result={0}", true && true);
-        Console.WriteLine("Operand1 = true, Operand2 = false, Result={0}", true && false);
-        Console.WriteLine("Operand1 = false, Operand2 = true, Result={0}", false && true);
-        Console.WriteLine("Operand1 = false, Operand2 = false, Result={0}", false && false);
+        Console.WriteLine("\nLogical OR operator - |");
+        Console.WriteLine("Operand1 = true, Operand2 = true, Result={0}", true | true);
+        Console.WriteLine("Operand1 = true, Operand2 = false, Result={0}", true | false);
+        Console.WriteLine("Operand1 = false, Operand2 = true, Result={0}", false | true);
+        Console.WriteLine("Operand1 = false, Operand2 = false, Result={0}", false | false);
 
 
         Console.ReadLine();
