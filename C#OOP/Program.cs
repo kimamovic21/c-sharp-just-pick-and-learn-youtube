9d2c75354d2a25677f9634b623dcc34031fb4ffd
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-24 16:08:49 +0100

    Parameterized Constructors in C#

--- C#OOP/Program.cs
+++ C#OOP/Program.cs
@@ -1,11 +1,30 @@
-﻿namespace Constructors
+﻿namespace Parameterized_Constructor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Constructors in C#");
-            Console.ReadLine();
+            Console.WriteLine("Parameterized Constructor in C# ");
+
+            // Creating object of Student class
+            Student student = new Student("Kerim", 28, 90.0);
+
+            // Create a Student object using the default constructor
+            Student student2 = new Student();
+
+            // Display student informations
+            Console.WriteLine("\nStudent informations:");
+            Console.WriteLine($"Name: {student.Name}");
+            Console.WriteLine($"Age: {student.Age}");
+            Console.WriteLine($"Grade: {student.Grade}");
+
+            // Display student2 informations
+            Console.WriteLine("\nStudent2 informations:");
+            Console.WriteLine($"Name: {student2.Name}");
+            Console.WriteLine($"Age: {student2.Age}");
+            Console.WriteLine($"Grade: {student2.Grade}");
+
+            Console.ReadKey();
         }
     }
 }
--- C#OOP/Student.cs
+++ C#OOP/Student.cs
@@ -1,10 +1,24 @@
-namespace Constructors
+namespace Parameterized_Constructor
 {
-    internal class Student
+    class Student
     {
         // Auto-implemented properties
-        public string Name { get; set; } // default value null
-        public int Age { get; set; }  // default value 0
-        public string Grade { get; set; }   // default value 0.0
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Grade { get; set; }
+
+        // Default Constructor
+        public Student()
+        {
+
+        }
+
+        // Parameterized Constructor
+        public Student(string name, int age, double grade)
+        {
+            Name = name;
+            Age = age;
+            Grade = grade;
+        }
     }
 }
