35cf5bae0b9807614ceb83e225c196b502899aee
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-24 15:30:26 +0100

    Properties in C#

--- C#OOP/Program.cs
+++ C#OOP/Program.cs
@@ -1,16 +1,16 @@
-﻿namespace Encapsulation
+﻿namespace Properties
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Encapsulation Example");
+            // Create a Student object
+            Student student1 = new Student("Kerim", 28, 90.5);
 
-            // Create an object of Employee
-            Employee employee1 = new Employee("John Doe", 1, 1000);
-
-            // Display the employee information
-            employee1.DisplayEmployeeInfo();
+            // Accessing properties
+            Console.WriteLine("Student's name: {0}", student1.Name);
+            Console.WriteLine("Student's age: {0}", student1.Age);
+            Console.WriteLine("Student's grade: {0}", student1.Grade);
 
             Console.ReadLine();
         }
