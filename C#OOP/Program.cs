f513140dbba86e80138ac435b60543f493233ee8
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-24 15:48:06 +0100

    Constructors in C#

--- C#OOP/Program.cs
+++ C#OOP/Program.cs
@@ -1,17 +1,10 @@
-﻿namespace Properties
+﻿namespace Constructors
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
-            // Create a Student object
-            Student student1 = new Student("Kerim", 28, 90.5);
-
-            // Accessing properties
-            Console.WriteLine("Student's name: {0}", student1.Name);
-            Console.WriteLine("Student's age: {0}", student1.Age);
-            Console.WriteLine("Student's grade: {0}", student1.Grade);
-
+            Console.WriteLine("Constructors in C#");
             Console.ReadLine();
         }
     }
--- C#OOP/Student.cs
+++ C#OOP/Student.cs
@@ -1,69 +1,10 @@
-namespace Properties
+namespace Constructors
 {
     internal class Student
     {
-        // Fields
-        private string _name;
-        private int _age;
-        private double _grade;
-
-        // Constructor
-        public Student(string name, int age, double grade)
-        {
-            Name = name;
-            Age = age;
-            Grade = grade;
-        }
-
-        // Property for student's name
-        public string Name
-        {
-            get { return _name; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                {
-                    _name = value;
-                }
-                else
-                {
-                    Console.WriteLine("Name cannot be null or empty.");
-                }
-            }
-        }
-
-        // Property for student's age
-        public int Age
-        {
-            get { return _age; }
-            set
-            {
-                if (value > 0)
-                {
-                    _age = value;
-                }
-                else
-                {
-                    Console.WriteLine("Age must be greater than 0.");
-                }
-            }
-        }
-
-        // Property for student's grade
-        public double Grade
-        {
-            get { return _grade; }
-            set
-            {
-                if (value >= 0 && value <= 100)
-                {
-                    _grade = value;
-                }
-                else
-                {
-                    Console.WriteLine("Grade must be between 0 and 100.");
-                }
-            }
-        }
+        // Auto-implemented properties
+        public string Name { get; set; } // default value null
+        public int Age { get; set; }  // default value 0
+        public string Grade { get; set; }   // default value 0.0
     }
 }
