1dc68d72d865035690be66d1971bf494af1d3702
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-27 16:10:56 +0100

    Polymorphism in C#

--- C#OOP/Program.cs
+++ C#OOP/Program.cs
@@ -1,21 +1,26 @@
-﻿namespace Inheritance
+﻿namespace Polymorphism
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Inheritance in C#");
+            Console.WriteLine("Polymorphism in C#");
 
-            // Creating an instance of the derived class (FictionBook)
-            FictionBook fictionBook = new FictionBook(
-                "Introduction to C#", 
-                "John Doe", 
-                100, 
-                "Science",
-                "lorem ipsum dolor sit amet"
-            );
-            fictionBook.DisplayInfo();
-            fictionBook.PrintDescription();
+            Student student1 = new Student()
+            {
+                Name = "Kerim",
+                Age = 28
+            };
+
+            // Call the overloaded methods
+            student1.DisplayInformation();
+            // Calls the first method
+
+            student1.DisplayInformation("Student Information");
+            // Calls the second method
+
+            student1.DisplayInformation("Student Information", 90);
+            // Calls the third method
 
             Console.ReadKey();
         }
--- C#OOP/Student.cs
+++ C#OOP/Student.cs
@@ -1,31 +1,23 @@
-namespace Copy_Constructor
+namespace Polymorphism
 {
     class Student
     {
         public string Name { get; set; }
         public int Age { get; set; }
-        public double Grade { get; set; }
 
-        // Parameterless constructor
-        public Student()
+        public void DisplayInformation()
         {
-
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
         }
 
-        // Parameterized constructor
-        public Student(string name, int age, double grade)
+        public void DisplayInformation(string message)
         {
-            Name = name;
-            Age = age;
-            Grade = grade;
+            Console.WriteLine($"{message} - Name: {Name}, Age: {Age}");
         }
 
-        // Copy constructor
-        public Student(Student student)
+        public void DisplayInformation(string message, int grade)
         {
-            Name = student.Name;
-            Age = student.Age;
-            Grade = student.Grade;
+            Console.WriteLine($"{message} - Name: {Name}, Age: {Age}, Grade: {grade}");
         }
     }
 }
