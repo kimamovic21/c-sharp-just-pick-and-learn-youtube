180834a6879cc600c5e203a67a345abdd721c238
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-29 13:21:01 +0100

    How to search for a specific string

--- C#ProgrammingChallenges/Program.cs
+++ C#ProgrammingChallenges/Program.cs
@@ -1,21 +1,32 @@
-﻿namespace FindStringLength
+﻿namespace StringSearch
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How to find length of a string \n");
+            Console.WriteLine("How to search for a specific string \n");
 
-            // Step 1 - Take the input from a user
-            Console.WriteLine("Enter a string");
-            string inputString = Console.ReadLine();
+            // Step 1: Ask the user to enter main senctence/string
+            Console.Write("Please enter main string/sentence: ");
+            string mainString = Console.ReadLine();
 
-            // Step 2 - Determine the length using Length property of a string
-            int length = inputString.Length;
+            // Step 2: Ask the user to enter search string/term
+            Console.Write("Enter the string to search: ");
+            string searchString = Console.ReadLine();
 
-            // Step 3 - Print the length on the console window
-            Console.WriteLine($"The length of the string is: {length}");
-                
+            // Step 3: Search for a string
+            bool found = mainString.Contains(searchString);
+
+            // Step 4: Print message on console window
+            if (found) 
+            {
+                Console.WriteLine("String found!");
+            }
+            else
+            {
+                Console.WriteLine("String not found!");
+            }
+
             Console.ReadKey();
         }
     }
