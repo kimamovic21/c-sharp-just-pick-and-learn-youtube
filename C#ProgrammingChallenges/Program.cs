6f3806c47e99761859bd70e210383f76d7561301
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-29 11:10:19 +0100

    How to extract a substring from a given string

--- C#ProgrammingChallenges/Program.cs
+++ C#ProgrammingChallenges/Program.cs
@@ -1,15 +1,33 @@
-﻿namespace FindCurrentDateAndTime
+﻿namespace ExtractSubstring
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How to get current date and time \n");
+            Console.WriteLine("How to extract a substring from a given string \n");
 
-            DateTime currentDateTime = DateTime.Now; 
-            // Returns an object whose values is the current local date and time
+            // Step 1 - Ask the user to enter a string
+            Console.WriteLine("Enter the string: ");
+            string inputString = Console.ReadLine();
 
-            Console.WriteLine(currentDateTime);
+            // Step 2 - Get the starting index
+            Console.WriteLine("Enter the starting index: ");
+            int startIndex = int.Parse(Console.ReadLine());
+
+            // Step 3 - Get the length of the substring
+            Console.WriteLine("Enter the length of the substring: ");
+            int length = int.Parse(Console.ReadLine());
+
+            // Step 4 - Extract the substring using Substring method
+            if (startIndex >= 0 && startIndex < inputString.Length && length >= 0)
+            {
+                string extractedSubstring = inputString.Substring(startIndex, length);
+                Console.WriteLine("Extracted substring: " + extractedSubstring);
+            } 
+            else 
+            {
+                Console.WriteLine("Invalid input. Please make sure the starting index and length are within the bounds of the input string");
+            }
 
             Console.ReadKey();
         }
