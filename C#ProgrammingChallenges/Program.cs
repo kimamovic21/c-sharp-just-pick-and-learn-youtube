697efbb371ed8d35d0b88aadf783b142f7d364b4
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-29 10:52:18 +0100

    How to remove white spaces from a string

---
+++ C#ProgrammingChallenges/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TrimWhiteSpaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How to remove white spaces from a string\n");
+
+            string string1 = "  Hello, World!  \n \n \n";
+            string trimmedString = string1.Trim();
+            string noSpaceString = trimmedString.Replace(" ", "");
+
+            Console.WriteLine(string1);
+            Console.WriteLine(trimmedString);
+            Console.WriteLine(noSpaceString);
+
+            Console.ReadKey();
+        }
+    }
+}
