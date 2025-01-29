ceb8d90796f81033ad127219ebed49e0d2793a75
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-29 16:05:15 +0100

    How to sort an Array

--- C#ProgrammingChallenges/Program.cs
+++ C#ProgrammingChallenges/Program.cs
@@ -1,26 +1,56 @@
-﻿namespace Search
+﻿namespace ArraySorting
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How to find element \n");
+            Console.WriteLine("How to sort an Array \n");
 
-            string[] fruits = { "Orange", "Apple", "Mango", "Watermelon", "Avocado" };
-            string searchTerm = "Watermelon";
-            //string searchTerm = "Banana";
+            int[] intArray = { 5, 4, 2, 1, 3 };
+            string[] stringArray = { "Orange", "Apple", "Mango", "Watermelon" };
 
-            // Searching for an element
-            string fruit = Array.Find(fruits, element => element.Equals(searchTerm));
-
-            if (!String.IsNullOrEmpty(fruit))
+            Console.WriteLine("Integer Array before sorting:");
+            foreach (int num in intArray)
             {
-                Console.WriteLine($"Search successful: {fruit}");
-            } else
-            {
-                Console.WriteLine("Fruit not found!");
+                Console.WriteLine(num);
             }
-            
+
+            Console.WriteLine("\nString Array before sorting:");
+            foreach (var str in stringArray)
+            {
+                Console.WriteLine(str);
+            }
+
+            // Sorting an array in ascending order
+            Array.Sort(intArray);
+            Console.WriteLine("\nInteger Array after ascending sorting:");
+            foreach (int num in intArray)
+            {
+                Console.WriteLine(num);
+            }
+
+            Array.Sort(stringArray);
+            Console.WriteLine("\nString Array after ascending sorting:");
+            foreach (var str in stringArray)
+            {
+                Console.WriteLine(str);
+            }
+
+            // Sorting an array in descending order
+            Array.Reverse(intArray);
+            Console.WriteLine("\nInteger Array after descending sorting:");
+            foreach (int num in intArray)
+            {
+                Console.WriteLine(num);
+            }
+
+            Array.Reverse(stringArray);
+            Console.WriteLine("\nString Array after descending sorting:");
+            foreach (var str in stringArray)
+            {
+                Console.WriteLine(str);
+            }
+
             Console.ReadKey();
         }
     }
