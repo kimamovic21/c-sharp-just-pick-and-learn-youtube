cd9291ce17526f1a4984fae257d79a67df1d49d5
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-29 15:40:23 +0100

    How to combine two arrays without duplicate values

--- C#ProgrammingChallenges/Program.cs
+++ C#ProgrammingChallenges/Program.cs
@@ -1,43 +1,35 @@
-﻿namespace ReadAndDisplayMatrix
+﻿namespace ArrayCombine
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How to read and display matrix \n");
+            Console.WriteLine("How to combine two arrays without duplicate values \n");
 
-            // Step 1: Propmpt user for number of rows and columns
-            Console.Write("Enter the number of rows: ");
-            int rows = int.Parse(Console.ReadLine());
-            Console.Write("Enter the number of columns: ");
-            int cols = int.Parse(Console.ReadLine());
+            int[] numbers1 = { 10,20,30,40,50 };
+            int[] numbers2 = { 40,50,60,70,80 };
 
-            // Step 2: Create the matrix
-            int[,] matrix = new int[rows, cols];
+            // Combine two arrays
+            int[] numbers = numbers1.Union(numbers2).ToArray();
 
-            // Step 3: Read values into the matrix
-            Console.WriteLine("Enter the values for the matrix: ");
+            Console.WriteLine("Array1:");
+            PrintArray(numbers1);
 
-            for (int i = 0; i < rows; i++) // rows
+            Console.WriteLine("\nArray2:");
+            PrintArray(numbers2);
+
+            Console.WriteLine("\nMerged array:");
+            PrintArray(numbers);
+
+            Console.ReadLine();
+        }
+
+        private static void PrintArray(int[] numbers)
+        {
+            foreach (var number in numbers)
             {
-                for (int j = 0; j < cols; j++) // columns
-                {
-                    Console.Write("Enter values for [{0}, {1}]: ", i, j);
-                    matrix[i, j] = int.Parse(Console.ReadLine());
-                }
+                Console.Write(number + ",");
             }
-
-            // Step 4: Display the matrix
-            for (int i = 0; i < rows; i++) // rows
-            {
-                for (int j = 0; j < cols; j++) // columns
-                {
-                    Console.Write("{0} ", matrix[i, j]);
-                }
-                Console.WriteLine();
-            }
-
-            Console.ReadKey();
         }
     }
 }
