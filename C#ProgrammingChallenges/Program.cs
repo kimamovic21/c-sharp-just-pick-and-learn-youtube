1c1879d059d1d560c1dfa8483a3258cd5659e20c
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-30 17:36:34 +0100

    Print the prime numbers

--- C#ProgrammingChallenges/Program.cs
+++ C#ProgrammingChallenges/Program.cs
@@ -1,52 +1,37 @@
-﻿namespace WriteTextToFile
+﻿namespace PrimeNumbers
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Write text to a file \n");
+            Console.WriteLine("Print the prime numbers \n");
 
-            const string path = @"C:\MyFiles\testFile.txt";
+            // Step 1: Ask the user to enter the limit
+            Console.WriteLine("Please enter the limit:");
+            int limit = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Please enter the text: \n");
-            string text = Console.ReadLine();
-
-            Console.WriteLine("Choose the option:");
-            Console.WriteLine("1. Overwrite the file");
-            Console.WriteLine("2. Append to the file");
-
-            bool isValidNumber = int.TryParse(Console.ReadLine(), out int selection);
-
-            if (!isValidNumber || selection < 1 || selection > 2)
+            // Step 2: Loop through all the numbers within the limit and check if a number is prime 
+            for (int i = 2; i < limit; i++)
             {
-                Console.WriteLine("\nYou have entered an invalid option. Try again.");
-            }
-            else
-            {
-                switch (selection)
+                bool isPrimeNumber = true;
+
+                for (int j = 2; j < i; j++)
                 {
-                    case 1:
-                        await WriteTextAsync(path, text, false);
-                        Console.WriteLine("Text has been overwritten.");
+                    if (i%j == 0)
+                    {
+                        isPrimeNumber = false;
                         break;
-                    case 2:
-                        await WriteTextAsync(path, text, true);
-                        Console.WriteLine("Text has been appended.");
-                        break;
-                    default:
-                        break;
+                    }
                 }
 
-                Console.WriteLine($"File location: {path}");
+                // Step 3: Print if it is a prime number
+                if (isPrimeNumber)
+                {
+                    Console.Write("{0} ", i);
+                }
             }
 
             Console.ReadKey();
-        }
-
-        public static async Task WriteTextAsync(string path, string text, bool append)
-        {
-            using StreamWriter file = new StreamWriter(path, append);
-            await file.WriteLineAsync(text);
         }
     }
 }
