832b3231d9ab6d22d3607a0ae8c762f16760eae1
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-30 17:29:27 +0100

    Write text to a file

--- C#ProgrammingChallenges/Program.cs
+++ C#ProgrammingChallenges/Program.cs
@@ -1,24 +1,52 @@
-﻿namespace ConcatenateTwoStrings
+﻿namespace WriteTextToFile
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("How to join two strings \n");
+            Console.WriteLine("Write text to a file \n");
 
-            // Step 1: Take the input
-            Console.WriteLine("Enter the first string: ");
-            string str1 = Console.ReadLine();
-            Console.WriteLine("Enter the second string: ");
-            string str2 = Console.ReadLine();
+            const string path = @"C:\MyFiles\testFile.txt";
 
-            // Step 2: Concatenate the strings
-            string str3 = string.Concat(str1, str2);
+            Console.WriteLine("Please enter the text: \n");
+            string text = Console.ReadLine();
 
-            // Step 3: Print the output
-            Console.WriteLine($"Your concatenated string: {str3}");
+            Console.WriteLine("Choose the option:");
+            Console.WriteLine("1. Overwrite the file");
+            Console.WriteLine("2. Append to the file");
+
+            bool isValidNumber = int.TryParse(Console.ReadLine(), out int selection);
+
+            if (!isValidNumber || selection < 1 || selection > 2)
+            {
+                Console.WriteLine("\nYou have entered an invalid option. Try again.");
+            }
+            else
+            {
+                switch (selection)
+                {
+                    case 1:
+                        await WriteTextAsync(path, text, false);
+                        Console.WriteLine("Text has been overwritten.");
+                        break;
+                    case 2:
+                        await WriteTextAsync(path, text, true);
+                        Console.WriteLine("Text has been appended.");
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine($"File location: {path}");
+            }
 
             Console.ReadKey();
+        }
+
+        public static async Task WriteTextAsync(string path, string text, bool append)
+        {
+            using StreamWriter file = new StreamWriter(path, append);
+            await file.WriteLineAsync(text);
         }
     }
 }
