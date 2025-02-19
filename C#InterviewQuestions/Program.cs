6f3e594872d8cb67ea4bc2e752724dceaf0f7cae
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-19 17:48:41 +0100

    Using keyword in C#

--- C#InterviewQuestions/Program.cs
+++ C#InterviewQuestions/Program.cs
@@ -1,32 +1,20 @@
-﻿namespace SampleConsoleApp
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace SampleConsoleApp
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            int num1 = 100;
-            int num2 = num1;
+            using StreamReader reader = File.OpenText(@"C:\MyFiles\test-file.txt");
+            
+            string fileContent = reader.ReadToEnd();
 
-            num1 = 200;
-
-            Console.WriteLine("num1: " + num1);
-            Console.WriteLine("num2: " + num2);
-
-            Person person1 = new Person();
-            person1.Name = "John";
-
-            Person person2 = person1;
-            person1.Name = "Kerim";
-
-            Console.WriteLine("person1.Name: " + person1.Name);
-            Console.WriteLine("person2.Name: " + person2.Name);
-
+            Console.WriteLine("File content:\n" + fileContent);
             Console.ReadLine();
         }
-    }
-
-    class Person
-    {
-        public string Name;
     }
 }
