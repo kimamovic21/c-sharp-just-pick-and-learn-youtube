fa9845a14a46c74d10c9fc4d05fc77959670d5dc
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-19 17:36:50 +0100

    Value and reference types in C#

---
+++ C#InterviewQuestions/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SampleConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 100;
+            int num2 = num1;
+
+            num1 = 200;
+
+            Console.WriteLine("num1: " + num1);
+            Console.WriteLine("num2: " + num2);
+
+            Person person1 = new Person();
+            person1.Name = "John";
+
+            Person person2 = person1;
+            person1.Name = "Kerim";
+
+            Console.WriteLine("person1.Name: " + person1.Name);
+            Console.WriteLine("person2.Name: " + person2.Name);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public string Name;
+    }
+}
